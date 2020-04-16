c437d5549f63e505e26cbc47ca19b07626d093ef
David Almeida <davidalmeidadrive@gmail.com>
2020-04-16 19:48:21 -0300

    first

--- hangman_game.py
+++ hangman_game.py
@@ -1,8 +1,50 @@
+import random
+
 
 def play():
     print("********************************")
     print("  WELCOME TO THE HANGMAN GAME! ")
     print("********************************")
+    print()
+
+    archive = open('words.txt', 'r')
+    words = []
+
+    for line in archive:
+        line = line.strip()
+        words.append(line)
+
+    archive.close()
+
+    number = random.randrange(0, len(words))
+    secret_word = words[number].upper()
+
+
+    hit_letter = ["_" for letter in secret_word] 
+
+    hanged = False
+    hit = False
+    error = 0
+    print(hit_letter)
+
+    while not hanged and not hit:
+
+        shot = input('type a letter: ')
+        shot = shot.strip().upper()
+
+        if shot in secret_word:
+            index = 0
+            for letter in secret_word:
+                if shot == letter:
+                    hit_letter[index] = letter
+                index += 1
+
+        else:
+            error += 1
+
+        hanged = error == 6  
+        hit = "_" not in hit_letter
+        print(hit_letter)
 
     print('End of the game')
 
